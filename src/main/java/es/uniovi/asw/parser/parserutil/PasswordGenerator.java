00dc120c724d3cacfd67a1fabf0e66e43d80095c
Roberto Pérez <robertops1818@gmail.com>
2018-05-10 16:57:19 +0200

    Contraseñas

--- src/main/java/es/uniovi/asw/parser/parserutil/PasswordGenerator.java
+++ src/main/java/es/uniovi/asw/parser/parserutil/PasswordGenerator.java
@@ -1,6 +1,5 @@
 package es.uniovi.asw.parser.parserutil;
 
-import java.util.Random;
 import java.util.Set;
 
 import es.uniovi.asw.agents.Agent;
@@ -11,14 +10,8 @@ public class PasswordGenerator {
 	private final static int passLength = 12;
 	
 	public static void createPasswords(Set<Agent> census) {
-		Random rnd = new Random();
-		StringBuilder sb = new StringBuilder(passLength);
 		for(Agent a: census) {
-			for (int i = 0; i < passLength; i++) {
-				sb.append(passCharacters
-						.charAt(rnd.nextInt(passCharacters.length())));
-			}
-			a.setPassword(sb.toString());
+			a.setPassword("123456");
 		}
 	}
 
